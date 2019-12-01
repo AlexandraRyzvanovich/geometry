7a61e484b52d8abdae2f42463305758910443b1f
Alexandra Ryzvanovich <Alexandraryzvanovich@gmail.com>
2019-12-01 17:51:12 +0300

    Creator Tests

--- src/test/java/utils/TriangleCreatorTest.java
+++ src/test/java/utils/TriangleCreatorTest.java
@@ -1,4 +1,29 @@
 package utils;
 
+import entity.Point;
+import entity.Triangle;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import validator.TrianglePointsValidator;
+import validator.TriangleValidator;
+
+import java.util.Optional;
+
 public class TriangleCreatorTest {
+    TriangleValidator validator = new TrianglePointsValidator();
+    TriangleCreator creator = new TriangleCreator(validator);
+
+    @Test
+    public void testCreatorShouldReturnIsPresentTriangleWhenValidLineGiven(){
+        String line = "1.2 88.5 14 55 89 10";
+        Optional<Triangle> triangle = creator.create(line);
+        Assert.assertTrue(triangle.isPresent());
+        Assert.assertTrue(triangle.get().getFirstPoint() == new Point(1.2,88.5));
+    }
+    @Test
+    public void testCreatorShouldReturnOptionalEmptyTriangleWhenLineInvalid(){
+        String line = "1.2 1.2 1.2 1.2 1.2";
+        Optional<Triangle> triangle = creator.create(line);
+        Assert.assertTrue(triangle.isPresent());
+    }
 }
